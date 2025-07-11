61df4b0ba918571554e4c05126bc0d7f99d1e6d4
Kanika Agarwal <93864492+Kanikagarwal@users.noreply.github.com>
2025-07-11 18:31:27 +0530

    third commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  server:{
-    proxy:{
-      '/api/auth':import.meta.env.VITE_SOCKET_URL,
-      '/api/code': import.meta.env.VITE_SOCKET_URL,
-    }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api/auth': env.VITE_SOCKET_URL,
+        '/api/code': env.VITE_SOCKET_URL,
+      },
+    },
   }
 })
